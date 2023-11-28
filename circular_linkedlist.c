eabd9b88542c9c61c2dcf6a9f9180a3daf79ff81
Aabel Thankachan <118375916+AabelThankachan@users.noreply.github.com>
2023-11-28 16:27:19 +0530

    Add files via upload

---
+++ circular_linkedlist.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *prev;
+  struct node *next;
+};
+
+struct node *front,*rear,*newnode,*temp;
+
+void push_front(int value)
+{
+   newnode=(struct node *)malloc(sizeof(struct node));
+   newnode->data=num;
+   newnode->next=front;
+     
